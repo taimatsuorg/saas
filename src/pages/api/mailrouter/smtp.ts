f6624ed37d26bbf36392a1806459c2a71114d2b1
QinYangWang <192667091@qq.com>
2025-04-15 01:42:09 +0800

    fix smtp mailrouter

--- src/pages/api/mailrouter/smtp.ts
+++ src/pages/api/mailrouter/smtp.ts
@@ -1,18 +1,41 @@
-import { sendEmail } from "@/lib/mailrouter";
 
-export async function POST({ locals, request }) {
-  const { API_TOKEN, DB } = locals.runtime.env;
+import { type WorkerMailerOptions, type EmailOptions, WorkerMailer, LogLevel } from "worker-mailer";
+
+export async function POST({ request }) {
 
   const body = await request.json();
   try {
-    const info = await sendEmail(body);
+    const config: WorkerMailerOptions = {
+      host: body.credential.host,
+      port: body.credential.port,
+      secure: body.credential.port == 465 ? true : false,
+      authType: 'login',
+      // logLevel: LogLevel.DEBUG,
+      credentials: {
+        username: body.credential.user,
+        password: body.credential.pass,
+      }
+    }
+    const email: EmailOptions = {
+      from: body.props.sender,
+      to: body.props.recipients,
+      subject: body.props.subject,
+      html: body.props.html,
+    }
+    await WorkerMailer.send(config, email)
     return Response.json(
-      { message: info, success: true },
+      { message: "success", success: true },
       { status: 201 },
     );
   } catch (error) {
+    if (error instanceof Error) {
+      return Response.json(
+        { message: error.message, success: false },
+        { status: 500 },
+      );
+    }
     return Response.json(
-      { message: error, success: false },
+      { message: `Internal server error`, success: false },
       { status: 500 },
     );
   }
